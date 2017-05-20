90a6dd5b5636ab63601718cd608ba579df09e4e7
tonystrawberry <tonyduong@live.fr>
2017-05-20 02:36:31 -0400

    Menu added

---
+++ Assets/Game Jam Template/Scripts/ShowPanels.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowPanels : MonoBehaviour {
+
+	public GameObject creditsPanel;							//Store a reference to the Game Object OptionsPanel 
+	public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
+
+
+	//Call this function to activate and display the Options panel during the main menu
+	public void ShowCreditsPanel()
+	{
+		creditsPanel.SetActive(true);
+	}
+
+	//Call this function to deactivate and hide the Options panel during the main menu
+	public void HideCreditsPanel()
+	{
+		creditsPanel.SetActive(false);
+	}
+
+	//Call this function to activate and display the main menu panel during the main menu
+	public void ShowMenu()
+	{
+		menuPanel.SetActive (true);
+	}
+
+	//Call this function to deactivate and hide the main menu panel during the main menu
+	public void HideMenu()
+	{
+		menuPanel.SetActive (false);
+	}
+
+}
