ad0143aa38d76cb3f601345838a308cca4ef9cb2
Peter Wilkinson <easeindesign@gmail.com>
2017-05-20 14:09:14 -0400

    ChangeEffect
    its pink, and encircles the character. Some debug size changes. Made
    with 2 particle emitters.

---
+++ Assets/Scripts/ChangeEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeEffect : MonoBehaviour {
+
+	private float minimum = 0.06f;
+	private float maximum = 2.0f;
+	static float t = 0.0f;
+
+	private ParticleSystem.MainModule ps;
+
+
+	// Use this for initialization
+	void Start () {
+		ps = gameObject.GetComponent<ParticleSystem> ().main;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ps.startSize = Mathf.Lerp (minimum, maximum, t);
+		t += 0.01f * Time.deltaTime;
+		if (t > 1.0f){
+			float temp = maximum;
+			maximum = minimum;
+			minimum = temp;
+			t = 0.0f;
+		}
+	}
+}
