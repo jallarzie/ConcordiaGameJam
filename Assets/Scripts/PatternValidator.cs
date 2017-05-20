f559443c663dd265c9ae4b6dc49a05c6b37e4f98
Audrey <jallarzie@gmail.com>
2017-05-20 16:00:18 -0400

    Detector following player

---
+++ Assets/Scripts/PatternValidator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Form
+{
+    Bunny,
+    Frog,
+    Fish
+}
+
+public enum MoveAction
+{
+    Hop,
+    NoHop
+}
+
+public class PatternValidator : MonoBehaviour {
+
+    [SerializeField]
+    private int _patternLenght;
+    [SerializeField]
+    private float _interval;
+
+    [SerializeField]
+    private MoveAction[][] _patterns;
+
+    private int _currentAction;
+
+    private void Start()
+    {
+        _currentAction = 0;
+
+        StartCoroutine(PatternLoop());
+    }
+
+    private IEnumerator PatternLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_interval);
+
+            _currentAction = (_currentAction + 1) % _patternLenght;
+        }
+    }
+
+    public MoveAction GetCurrentActionForForm(Form form)
+    {
+        return _patterns[(int)form][_currentAction];
+    }
+}
