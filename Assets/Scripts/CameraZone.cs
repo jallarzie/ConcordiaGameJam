483abeb77452bdcac0c1420be3e2da15931dd49c
Audrey <jallarzie@gmail.com>
2017-05-19 21:01:26 -0400

    Camera Zones

---
+++ Assets/Scripts/CameraZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZone : MonoBehaviour 
+{
+    [SerializeField]
+    private Transform _cameraPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody.CompareTag("Player"))
+        {
+            Camera.main.transform.position = _cameraPoint.position;
+            Camera.main.transform.localRotation = _cameraPoint.localRotation;
+        }
+    }
+}
