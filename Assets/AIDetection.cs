ce3c47934bceac6f71cd188e1f97a2991ec2d2e0
tonystrawberry <tonyduong@live.fr>
2017-05-20 16:52:16 -0400

    Player goes out of padTest when wrong input pattern

---
+++ Assets/AIDetection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDetection : MonoBehaviour {
+
+    public float visionDistance = 2f;
+
+    private void FixedUpdate()
+    {
+        CheckFieldOfView();
+    }
+
+    private void CheckFieldOfView()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        Vector3 vectorBetweenGuardAndPlayer = player.transform.position - transform.position;
+        Vector3 localForward = transform.forward;
+        RaycastHit hit;
+
+        bool firstObjectRayIntersect = Physics.Raycast(transform.position, vectorBetweenGuardAndPlayer, out hit, Mathf.Infinity);
+        if (firstObjectRayIntersect)
+        {
+            if (hit.transform.tag != "Player")
+            {
+                return;
+            }
+            else
+            {
+                // Debug.Log("Player in field of view");
+            }
+        }
+        float distance = vectorBetweenGuardAndPlayer.magnitude;
+
+        // if player is close enough
+        if (distance < visionDistance)
+        {
+            float angle = Vector3.Angle(localForward, vectorBetweenGuardAndPlayer);
+            if (angle < 20f)
+            {
+                Debug.Log("Player detected");
+            }
+        }
+    }
+}
