ede6ac0361f404866f8cc55c31c31daf8dae6d0c
Peter Wilkinson <easeindesign@gmail.com>
2017-05-21 13:51:36 -0400

    Is it a party?….has an end.

---
+++ Assets/MusicTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicTrigger : MonoBehaviour {
+
+
+//	public AudioSource audio;
+//	public AudioClip victoryAudio;
+	// Use this for initialization
+	void Start () {
+//		audio.clip = victoryAudio;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log (audio.volume);
+		GameObject.Find("BackgroundAudio").GetComponent<AudioSource>().volume = 0;
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+//			audio.clip = victoryAudio;
+			SceneManager.LoadScene(0);
+		}
+	}
+
+}
