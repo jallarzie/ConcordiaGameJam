237a244dc0ec94614d2d3685b2656c4a4cc3ad8e
Audrey <jallarzie@gmail.com>
2017-05-21 13:50:19 -0400

    GameManager no longer indestructable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -34,7 +34,6 @@ public class GameManager : MonoBehaviour {
             instance = this;
         else if (instance != this)
             Destroy(gameObject);
-        DontDestroyOnLoad(gameObject);
     }
 
     void Start () {
